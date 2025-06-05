d8565ee996d5c81134c8bc348103f9f530b94cd0
saidulislam28 <saidulislam79028@gmail.com>
2025-06-05 16:45:21 +0600

    designed banner with static date

--- website/src/app/(common-pages)/page.tsx
+++ website/src/app/(common-pages)/page.tsx
@@ -1,23 +1,9 @@
-// import { GET_TASK } from "@/api/endpoints";
-// import { get } from "../utils/ApiCalls";
-// import TaskButton from "@/component/taskButton";
-
-// interface TASK {
-//   id: number;
-//   title: string;
-//   sub_title: string;
-// }
+import Banner from "@/component/home/Banner";
 
 export default async function Home() {
-  // const tasks: TASK[] = await get(GET_TASK);
-  // console.log(tasks);
-
   return (
-    <div className="max-w-7xl mx-auto">
-      {/* {tasks.map((task) => (
-        <TaskButton task={task} key={task?.id} />
-      ))} */}
-      this is Home
+    <div className="mx-auto">
+      <Banner />
     </div>
   );
 }
